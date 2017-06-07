a9ad3aea78b4792d05f539184aa92872cd6ae6bc
jovargas <racing.tuner@gmail.com>
2017-06-07 16:16:27 -0300

    Add files via upload

---
+++ DetectionMaskTool/DetectionMaskForm.cs
@@ -0,0 +1,339 @@
+﻿/*  
+    Detection Mask Tool for OpenALPR 
+    Created by Joel Vargas
+    CC 2017    
+ */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using Emgu.CV.Util;
+
+namespace DetectionMaskTool
+{
+    public partial class DetectionMaskForm : Form
+    {
+        private List<List<Point>> Polygons;
+        private List<Point> NewPolygon;
+        private Point NewPoint;
+        Image referenceImage;
+        Image<Gray, byte> maskImage;
+        public float scaleFactor;
+        public DetectionMaskForm()
+        {
+            InitializeComponent();            
+        }
+
+        private void DetectionMaskForm_Load(object sender, EventArgs e)
+        {
+            Polygons = new List<List<Point>>();
+            NewPolygon = null;
+            NewPoint = new Point();
+            referenceImage = null;
+            maskImage = null;                  
+        }
+
+        private void pcbMask_MouseClick(object sender, MouseEventArgs e)
+        {         
+        }
+
+        private void pcbMask_MouseDown(object sender, MouseEventArgs e)
+        {
+            // See if we are already drawing a polygon.
+            if (NewPolygon != null)
+            {
+                // We are already drawing a polygon.
+                // If it's the right mouse button, finish this polygon.
+                if (e.Button == MouseButtons.Right)
+                {
+                    // Finish this polygon.
+                    if (NewPolygon.Count > 2) Polygons.Add(NewPolygon);
+                    NewPolygon = null;
+                }
+                else
+                {
+                    // Add a point to this polygon.
+                    if (NewPolygon[NewPolygon.Count - 1] != e.Location)
+                    {
+                        NewPolygon.Add(e.Location);
+                    }
+                }
+            }
+            else
+            {
+                // Start a new polygon.
+                NewPolygon = new List<Point>();
+                NewPoint = e.Location;
+                NewPolygon.Add(e.Location);
+            }
+
+            // Redraw.
+            pcbMask.Invalidate();
+        }
+
+        private void pcbMask_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (NewPolygon == null) return;
+            NewPoint = e.Location;
+            pcbMask.Invalidate();
+        }
+
+        private void pcbMask_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            //e.Graphics.Clear(pcbMask.BackColor);
+
+            // Draw the old polygons.
+            foreach (List<Point> polygon in Polygons)
+            {
+                SolidBrush semiTransBrush = new SolidBrush(Color.FromArgb(128, 0, 0, 255));
+                e.Graphics.FillPolygon(semiTransBrush, polygon.ToArray(), FillMode.Alternate);
+                e.Graphics.DrawPolygon(new Pen(Color.Magenta, 2), polygon.ToArray());
+            }
+
+            // Draw the new polygon.
+            if (NewPolygon != null)
+            {
+                // Draw the new polygon.
+                if (NewPolygon.Count > 1)
+                {                    
+                    e.Graphics.DrawLines(new Pen(Color.Magenta, 2), NewPolygon.ToArray());
+                }
+
+                // Draw the newest edge.
+                if (NewPolygon.Count > 0)
+                {
+                    using (Pen dashed_pen = new Pen(Color.Magenta))
+                    {
+                        dashed_pen.DashPattern = new float[] { 3, 3 };
+                        dashed_pen.Width = 2;
+                        e.Graphics.DrawLine(dashed_pen, NewPolygon[NewPolygon.Count - 1], NewPoint);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {                                        
+            DialogResult dialogResult = MessageBox.Show("Save the mask?", "Save confirmation", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                if (Polygons.Count > 0)
+                    maskImage = new Image<Gray, byte>(pcbMask.Width, pcbMask.Height, new Gray(0.0));
+                else
+                    maskImage = new Image<Gray, byte>(pcbMask.Width, pcbMask.Height, new Gray(255));
+                foreach (var polygon in Polygons)
+                {
+                    Point[] points = new Point[polygon.Count];
+                    for (int i = 0; i < polygon.Count; i++)
+                    {
+                        points[i] = polygon.ElementAt(i);
+                    }
+                    maskImage.Draw(points, new Gray(255), -1, LineType.AntiAlias, default(Point));
+                }
+                maskImage = maskImage.Resize(referenceImage.Width, referenceImage.Height, Inter.Linear);
+                Rectangle roi = GetMaskROI(maskImage.Clone());
+                var referenceImg = new Image<Bgr, byte>((Bitmap)referenceImage);
+                var referenceImg2 = new Image<Bgr, byte>((Bitmap)referenceImage);
+                CvInvoke.Rectangle(referenceImg, roi, new MCvScalar(255, 0, 255), 2);
+                if (chbxDebugImgs.Checked)
+                {
+                    CvInvoke.Imshow("Mask ROI - Size: " + roi.Width.ToString() + "x" + roi.Height.ToString(), referenceImg);
+                    ShowMaskedImage(referenceImg2, maskImage, roi);
+                }                
+                saveFileDialog1.ShowDialog();
+                if (saveFileDialog1.FileName != "mask.jpg")
+                {                    
+                    FileStream fs = (FileStream)saveFileDialog1.OpenFile();
+                    maskImage.ToBitmap().Save(fs, ImageFormat.Jpeg);
+                    fs.Close();
+                }                
+            }          
+        }
+
+        private void saveJpeg(string path, Image img, long quality)
+        {
+            // Encoder parameter for image quality
+
+            EncoderParameter qualityParam = new EncoderParameter(Encoder.Quality, quality);
+
+            // Jpeg image codec
+            ImageCodecInfo jpegCodec = getEncoderInfo("image/jpeg");
+
+            if (jpegCodec == null)
+                return;
+
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+
+            img.Save(path, jpegCodec, encoderParams);
+        }
+        private ImageCodecInfo getEncoderInfo(string mimeType)
+        {            
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+                        
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            return null;
+        }
+
+        delegate void SetPicBoxMaskCallback(Image img);
+        public void SetPicBoxMaskImg(Image img)
+        {
+            if (pcbMask.InvokeRequired)
+            {
+                SetPicBoxMaskCallback d = new SetPicBoxMaskCallback(SetPicBoxMaskImg);
+                Invoke(d, new object[] { img });
+            }
+            else
+            {
+                pcbMask.Image = img;
+                pcbMask.Refresh();
+            }
+        }
+
+        delegate void SetPicBoxMaskSizeCallback(int width, int height);
+        private void SetPicBoxMaskSize(int width, int height)
+        {
+            if (pcbMask.InvokeRequired)
+            {
+                SetPicBoxMaskSizeCallback d = new SetPicBoxMaskSizeCallback(SetPicBoxMaskSize);
+                Invoke(d, new object[] { width, height });
+            }
+            else
+            {
+                pcbMask.Size = new Size(width,height);                
+            }
+        }
+        delegate void SetFormSizeCallback(int width, int height);
+        private void SetFormSize(int width, int height)
+        {
+            if (pcbMask.InvokeRequired)
+            {
+                SetFormSizeCallback d = new SetFormSizeCallback(SetFormSize);
+                Invoke(d, new object[] { width, height });
+            }
+            else
+            {
+                this.Size = new Size(width, height);
+            }
+        }
+        private void btnClearMask_Click(object sender, EventArgs e)
+        {            
+            Polygons = new List<List<Point>>();
+            pcbMask.Refresh();            
+        }
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                referenceImage = new Bitmap(openFileDialog1.OpenFile());
+                Polygons = new List<List<Point>>();
+                pcbMask.Refresh();
+            }
+            else
+            {
+                return;
+            }
+            scaleFactor = ((float)referenceImage.Width / (float)referenceImage.Height);
+            SetPicBoxMaskImg(referenceImage);            
+            btnClearMask.Enabled = true;
+            btnSave.Enabled = true;            
+        }
+        private Rectangle GetMaskROI(Image<Gray, byte> mask)
+        {
+            Rectangle roi = new Rectangle();
+            CvInvoke.Threshold(mask, mask, 55, 255, ThresholdType.Binary);
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            //var hierarchy = new Mat();
+            //double largest_area = 0;            
+            CvInvoke.FindContours(mask, contours, null, RetrType.Ccomp, ChainApproxMethod.ChainApproxSimple);
+            // iterate through each contour.
+            int right = 0, left = 999999, top = 9999999, bottom = 0;
+            for (int i = 0; i < contours.Size; i++)
+            {
+
+                ////  Find the area of contour
+                //double a = CvInvoke.ContourArea(contours[i], false);
+                ////if (a > largest_area)                
+                //{
+                //    // Find the bounding rectangle for biggest contour
+                //    roi = CvInvoke.BoundingRectangle(contours[i]);
+                //}
+                roi = CvInvoke.BoundingRectangle(contours[i]);
+                if (roi.Y < top)
+                    top = roi.Y;
+                if (roi.X < left)
+                    left = roi.X;
+                if ((roi.X + roi.Width) > right)
+                    right = roi.X + roi.Width;
+                if ((roi.Y + roi.Height) > bottom)
+                    bottom = roi.Y + roi.Height;
+            }
+            roi = new Rectangle(left, top, right - left, bottom - top);
+            //===================================================
+            // Calculate the biggest rectangle that covers all the whitespace
+            // Rather than using contours, go row by row, column by column until you hit
+            // a white pixel and stop.  Should be faster and a little simpler
+            //int top_bound = 0, bottom_bound = 0, left_bound = 0, right_bound = 0;
+            //Mat maskMat = new Mat(mask.Mat, mask.ROI);
+            //int i;
+            //for (top_bound = 0; top_bound < maskMat.Rows; top_bound++)
+            //    if (CvInvoke.CountNonZero(maskMat.GetRow(top_bound)) > 0) break;
+            //for (bottom_bound = maskMat.Rows - 1; bottom_bound >= 0; bottom_bound--)
+            //    if (CvInvoke.CountNonZero(maskMat.GetRow(bottom_bound)) > 0) break;            
+            //for (left_bound = 0; left_bound < maskMat.Cols; left_bound++)
+            //    if (CvInvoke.CountNonZero(maskMat.) > 0) break;            
+            //for (right_bound = maskMat.Cols - 1; right_bound >= 0; right_bound--)
+            //    if (CvInvoke.CountNonZero(mask.GetCol(right_bound)) > 0) break;
+
+            //if (left_bound >= right_bound || top_bound >= bottom_bound)
+            //{
+            //    // Invalid mask, set it to 0 width/height
+            //    roi.X = 0;
+            //    roi.Y = 0;
+            //    roi.Width = 0;
+            //    roi.Height = 0;
+            //}
+            //else
+            //{
+            //    roi.X = left_bound;
+            //    roi.Y = top_bound;
+            //    roi.Width = right_bound - left_bound;
+            //    roi.Height = bottom_bound - top_bound;
+            //}
+            ////===================================================
+
+            return roi;
+        }
+        private void ShowMaskedImage(Image<Bgr, byte> img, Image<Gray, byte> mask, Rectangle maskRoi)
+        {
+            Image<Bgr, float> maskF = mask.Convert<Bgr, float>();            
+            Image<Bgr, float> imageF = img.Convert<Bgr, float>();
+            img = imageF.Mul(maskF).Convert<Bgr, byte>();
+
+            CvInvoke.Imshow("Mask", mask);
+            img.ROI = maskRoi;            
+
+            CvInvoke.Imshow("Masked image", img);
+            
+            maskF.Dispose();            
+            imageF.Dispose();
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure?", "Close confirmation", MessageBoxButtons.YesNo);
+            if(dialogResult == DialogResult.Yes)
+                Close();
+        }
+    }
+}
---
+++ DetectionMaskTool/DetectionMaskForm.designer.cs
@@ -0,0 +1,220 @@
+﻿namespace DetectionMaskTool
+{
+    partial class DetectionMaskForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbMask = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClearMask = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.chbxDebugImgs = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMask)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pcbMask
+            // 
+            this.pcbMask.BackColor = System.Drawing.Color.DimGray;
+            this.pcbMask.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tableLayoutPanel1.SetColumnSpan(this.pcbMask, 6);
+            this.pcbMask.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pcbMask.Location = new System.Drawing.Point(3, 33);
+            this.pcbMask.Name = "pcbMask";
+            this.pcbMask.Size = new System.Drawing.Size(884, 465);
+            this.pcbMask.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbMask.TabIndex = 0;
+            this.pcbMask.TabStop = false;
+            this.pcbMask.Paint += new System.Windows.Forms.PaintEventHandler(this.pcbMask_Paint);
+            this.pcbMask.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pcbMask_MouseClick);
+            this.pcbMask.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pcbMask_MouseDown);
+            this.pcbMask.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pcbMask_MouseMove);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(203, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClearMask
+            // 
+            this.btnClearMask.Enabled = false;
+            this.btnClearMask.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnClearMask.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClearMask.ForeColor = System.Drawing.Color.White;
+            this.btnClearMask.Location = new System.Drawing.Point(103, 3);
+            this.btnClearMask.Name = "btnClearMask";
+            this.btnClearMask.Size = new System.Drawing.Size(94, 23);
+            this.btnClearMask.TabIndex = 3;
+            this.btnClearMask.Text = "Clear Mask";
+            this.btnClearMask.UseVisualStyleBackColor = true;
+            this.btnClearMask.Click += new System.EventHandler(this.btnClearMask_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "mask.jpg";
+            this.saveFileDialog1.Filter = "JPG Image|*.jpg";
+            this.saveFileDialog1.Title = "Save mask";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.bmp;*.png";
+            this.openFileDialog1.Title = "Open image to create the mask";
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnLoadImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoadImage.ForeColor = System.Drawing.Color.White;
+            this.btnLoadImage.Location = new System.Drawing.Point(3, 3);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(94, 23);
+            this.btnLoadImage.TabIndex = 4;
+            this.btnLoadImage.Text = "Load Image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pcbMask, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnSave, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnClearMask, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnLoadImage, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnClose, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.chbxDebugImgs, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 4, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(890, 501);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(793, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(94, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // chbxDebugImgs
+            // 
+            this.chbxDebugImgs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chbxDebugImgs.AutoSize = true;
+            this.chbxDebugImgs.ForeColor = System.Drawing.Color.White;
+            this.chbxDebugImgs.Location = new System.Drawing.Point(303, 3);
+            this.chbxDebugImgs.Name = "chbxDebugImgs";
+            this.chbxDebugImgs.Size = new System.Drawing.Size(94, 24);
+            this.chbxDebugImgs.TabIndex = 5;
+            this.chbxDebugImgs.Text = "Debug images";
+            this.chbxDebugImgs.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(403, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "OpenALPR Mask Creator Tool v2.1 (by Joel Vargas)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DetectionMaskForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(890, 501);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DetectionMaskForm";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OpenALPR - Detection Mask Creator v1.0 (by Joel Vargas)";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.DetectionMaskForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMask)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbMask;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClearMask;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.CheckBox chbxDebugImgs;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
